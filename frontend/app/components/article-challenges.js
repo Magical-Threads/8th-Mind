a4d964d4878149be0aa3785869d800789ef2ee27
Michael Latta (TechnoMage) <mlatta@technomage.com>
2017-12-21 12:32:43 -0700

    Present login link on challenge page
    If the user is not logged in, redirect to login before allowing submissions

--- frontend/app/components/article-challenges.js
+++ frontend/app/components/article-challenges.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 import config from './../config/environment';
 
 export default Ember.Component.extend({
+	session: Ember.inject.service('session'),
 	serverURL: config.serverPath,
 	classNames: [
 		'article-challenge'
