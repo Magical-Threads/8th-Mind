16ac02d8486c04f189baefab4615d261da86f9e2
Michael Latta (TechnoMage) <mlatta@technomage.com>
2017-12-22 08:34:26 -0700

    Remove debug

--- server/models/article.js
+++ server/models/article.js
@@ -62,7 +62,7 @@ class Article extends Base {
     qparams.unshift(d.getFullYear()+'.'+(d.getMonth()+1)+'.'+d.getDate());
 
   	// console.warn("@@@@@@ tag: ",tag," phrase: ",tag_phrase);
-    console.warn('@@@@ Time filter: ',time,' params: ',qparams)
+    // console.warn('@@@@ Time filter: ',time,' params: ',qparams)
 
   	// todo: determine if we still need a left join here (will there ever be orphan articles with no userID?) -RJD
   	let query = "SELECT articles.articleID, articles.articleTitle, "+
